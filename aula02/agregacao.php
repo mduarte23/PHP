f53fee773ca99f4e09ce262655a574501bd5232f
Marcelo <marceloferraira@hotmail.com>
2024-04-17 19:26:55 -0300

    update

---
+++ aula02/agregacao.php
@@ -0,0 +1,24 @@
+<?php
+include_once 'classes/Cesta.class.php';
+include_once 'classes/Produto.class.php';
+
+$produto1 = new Produto;
+$produto2 = new Produdo;
+$produto3 = new Produto;
+$produto4 = new Produto;
+
+$produto1->Codigo = 1;
+$produto1->Descricao = 'Ameixa';
+$produto1->Preco = 1.40;
+
+$produto2->Codigo = 2;
+$produto2->Descricao = 'Morango';
+$produto2->Preco = 2.24;
+
+$produto3->Codigo = 3;
+$produto3->Descricao = 'Abacaxi';
+$produto3->Preco = 2.86;
+
+$produto4->Codigo = 4;
+$produto4->Descricao = 'Laranja';
+$produto4->Preco = 1.14;
